1f528440e56b4d4d0456f3108b627c4ae58aea62
Manthan2005abc <hemangi1683@gmail.com>
2020-05-29 04:10:35 +0000

    Yo

---
+++ index.js
@@ -0,0 +1,31 @@
+/////////testingo
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log('RUNING YOUR SERVER ...');
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+function newConnection(socket) {
+	console.log('new connection : ' + socket.id);
+
+	socket.on('data', print);
+	socket.on('mouse', drw);
+	function print(data) {
+		console.log(data);
+		socket.broadcast.emit('data', data);
+		//io.sockets.emit('data',datasdggg); }}
+	}
+	function drw(d) {
+		console.log('drawing');
+		socket.broadcast.emit('mouse', d);
+	}
+}
--- public/sketch.js
+++ public/sketch.js
@@ -1,102 +1,94 @@
-var socket;
-var c=0;
-var c1=0;
-var msg=[''];
-var name="UNKNOWN";
+c1 = 0;
+var msg = [''];
+var name = 'UNKNOWN';
 let inp;
 var mouse;
-var data={
-msg:"connected",name:"unknown"
-
+var data = {
+	msg: 'connected',
+	name: 'unknown'
 };
 let button;
-function setup()
-{
- createCanvas(400,400);
- socket = io.connect('http://localhost:3000');
- socket.on('data',todo);
- socket.on('mouse',drw);
- console.log("enter name");
+function setup() {
+	createCanvas(400, 400);
+	// you set this to io.connect('localhost:3000')
+	socket = io.connect('https://chattest--manthan2005abc.repl.co');
+	socket.on('data', todo);
+	socket.on('mouse', drw);
+	console.log('enter name');
 
-  button = createButton('send');
-  button.position(width-100, height-100);
-  button.mousePressed(changeBG);
+	button = createButton('send');
+	button.position(width - 100, height - 100);
+	button.mousePressed(changeBG);
 
-  button = createButton('clear');
-  button.position(width-100, height-60);
-  button.mousePressed(clear);
+	button = createButton('clear');
+	button.position(width - 100, height - 60);
+	button.mousePressed(clear);
 
- inp = createInput('');
- inp.input(myInputEvent);
-
+	inp = createInput('');
+	inp.input(myInputEvent);
 }
 
-
-
-function todo(dat){
-	console.log(c+" : "+ dat.name +" : "+dat.msg);
-  text(c+' : '+ dat.name +' : '+dat.msg,0,c*15);
-  c++;
+function todo(dat) {
+	console.log(c + ' : ' + dat.name + ' : ' + dat.msg);
+	text(c + ' : ' + dat.name + ' : ' + dat.msg, 0, c * 15);
+	c++;
 }
 
-function draw()
-{
-  fill(80,0,0);
-  ellipse(mouseX,mouseY,50,50);
-  fill(90);
-  textSize(12);
-//  text('Georgia', 120, 300);
-  // console.log(msg +"++++"+ data.msg);
+function draw() {
+	fill(80, 0, 0);
+	ellipse(mouseX, mouseY, 50, 50);
+	fill(90);
+	textSize(12);
+	//  text('Georgia', 120, 300);
+	// console.log(msg +"++++"+ data.msg);
 }
 
+function keyPressed() {
+	entered(key);
+}
+function entered(key) {
+	if (key == 'Enter') {
+		if (c1 == 0) {
+			name = inp.elt.value;
+		}
+		data = {
+			msg: inp.elt.value,
+			name: name
+		};
+		socket.emit('data', data);
+		console.log(c + ' you : ' + data.msg + '   u r name ' + data.name);
 
-function keyPressed()
-{entered(key);}
-function entered(key){
-  if(key=='Enter')
-  {
-    if(c1==0){name=inp.elt.value;}
-    data={
-      msg:inp.elt.value,
-      name:name
-         }
-    socket.emit('data',data);
-    console.log(c+" you : "+data.msg+"   u r name "+data.name);
-
-    //  msg = [''];
-      c++;
-      c1++;
-      msg=data.msg;
-}}
+		//  msg = [''];
+		c++;
+		c1++;
+		msg = data.msg;
+	}
+}
 function changeBG() {
-  let val = random(255);
-  background(val,random(255),random(255));
-  entered('Enter');
+	let val = random(255);
+	background(val, random(255), random(255));
+	entered('Enter');
 }
 
-function myInputEvent()
-{
-  console.log('you are typing: ') ;
- //msg=this.value();
+function myInputEvent() {
+	console.log('you are typing: ');
+	//msg=this.value();
 }
 
-function clear(){
-
-c=1;
-
+function clear() {
+	c = 1;
 }
-function mouseDragged()
-{
-mouse={
-  mx:mouseX,
-  my:mouseY}
+function mouseDragged() {
+	mouse = {
+		mx: mouseX,
+		my: mouseY
+	};
 
-  socket.emit('mouse',mouse);
-
+	socket.emit('mouse', mouse);
 }
 
-function drw(mous){
-  fill(30,0,0);
-  ellipse(mous.mx,mous.my,50,50)
-  console.log("drawing");
+function drw(mous) {
+	fill(30, 0, 0);
+	ellipse(mous.mx, mous.my, 50, 50);
+	console.log('drawing');
 }
