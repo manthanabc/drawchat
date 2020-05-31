e10094ea0c807aa183b2678228c809d8e730536d
manthanabc <manthanmji@gmail.com>
2020-05-31 11:17:56 +0530

    no change

--- public/sketch.js
+++ public/sketch.js
@@ -1,100 +1,70 @@
-c1 = 0;
+var socket;
 var c=0;
-var msg = [''];
-//var name ;//= 'UNKNOWN';
-let inp;
-var mouse;
-var data = {
-	msg: 'connected',
-	name: 'unknown'
-};
+var msg=[''];
+var name="UNKNOWN";
+var data;
 let button;
-function setup() {
-	createCanvas(400, 400);
-	// you set this to io.connect('localhost:3000')
-	socket = io.connect('https://chattest--manthan2005abc.repl.co');
-	socket.on('data', todo);
-	socket.on('mouse', drw);
-	console.log('enter name');
+function setup()
+{
+ createCanvas(400,400);
+ socket = io.connect('http://localhost:3000');
+ socket.on('data',todo);
+ console.log("enter name");
 
-	button = createButton('send');
-	button.position(width - 100, height - 100);
-	button.mousePressed(changeBG);
+ button = createButton('click me');
+  button.position(19, 19);
+  button.mousePressed(changeBG);
+  let inp = createInput('');
+ inp.input(myInputEvent);
 
-	button = createButton('clear');
-	button.position(width - 100, height - 60);
-	button.mousePressed(clear);
-
-	inp = createInput('');
-	inp.input(myInputEvent);
 }
 
-function todo(dat) {
-  //background(0);
-//	console.log(c + ' : ' + dat.name + ' : ' + dat.msg);
-	text(dat.name + ' : ' + dat.msg, 100,c*10);
-	c++;
+
+function todo(data){
+	console.log(c+" : "+ data.name +" : "+data.msg);
+  text(c+' : '+ data.name +' : '+data.msg,0,c*15);
 }
 
-function draw() {
-	fill(80, 0, 0);
-	ellipse(mouseX, mouseY, 10, 10);
-	fill(90);
-	textSize(12);
-  text('made by Manthan', 120, 300);
-  text(c1, 0, 0);
-	// console.log(msg +"++++"+ data.msg);
-	fill(80,0,0);
-	ellipse(mouseX, mouseY, 10,10) ;
+function draw()
+{
+  fill(80,0,0);
+  ellipse(mouseX,mouseY,100,50);
+  fill(90);
+  textSize(12);
+//  text('Georgia', 120, 300);
+
 }
 
-function keyPressed() {
-	entered(key);
-}
-function entered(key) {
-	if (key == 'Enter') {
-		if (c1 === 0) {
-			//name = inp.elt.value;
 
-		data = {
-			name:inp.elt.value
-		};
-		}
-		data.msg=inp.elt.value;
-		socket.emit('data', data);
-		console.log(c + ' you : ' + data.msg + '   u r name ' + data.name);
+function keyPressed()
+{entered(key);}
+function entered(key){
 
-		//  msg = [''];
-		c++;
-		c1++;
-		msg = data.msg;
-	}
+  if(key=='Enter')
+  {
+    if(c==0){name=msg.join('');msg = [''];}
+
+     data={
+       msg:msg.join(''),
+       name:name
+           }
+        socket.emit('data',data);
+
+      console.log(c+" you : "+msg.join(''));
+      msg = [''];
+      c++;
+  }else{
+
+   append(msg, key);
+
+       }
 }
 function changeBG() {
-	let val = random(255);
-//	background(val, random(255), random(255));
-	entered('Enter');
+  let val = random(255);
+  background(val,random(255),random(255));
+  entered('enter');
 }
 
 function myInputEvent() {
-	console.log('you are typing: ');
-	//msg=this.value();
-}
-
-function clear() {
-	c = 1;
-}
-function mouseDragged() {
-	mouse = {
-		mx: mouseX,
-		my: mouseY
-	};
-
-	socket.emit('mouse', mouse);
-}
-
-function drw(mous) {
-	fill(30, 0, 0);
-	ellipse(mous.mx, mous.my, 10, 10);
-	console.log('drawing');
+  console.log('you are typing: ', this.value());
 }
