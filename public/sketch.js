9a60be265579cfc9fb640265fa668b8d9e413d3e
manthanabc <manthanmji@gmail.com>
2020-05-31 11:20:42 +0530

    thats it

--- public/sketch.js
+++ public/sketch.js
@@ -1,70 +1,132 @@
-var socket;
+c1 = 0;
+var click=0;
 var c=0;
-var msg=[''];
-var name="UNKNOWN";
-var data;
+var msg = [''];
+//var name ;//= 'UNKNOWN';
+let inp;
+var mouse;
+var data = {
+	msg: 'connected',
+	name: 'unknown'
+};
 let button;
-function setup()
-{
- createCanvas(400,400);
- socket = io.connect('http://localhost:3000');
- socket.on('data',todo);
- console.log("enter name");
+function setup() {
+	createCanvas(400, 400);
+	// you set this to io.connect('localhost:3000')
+	socket = io.connect('https://chattest--manthan2005abc.repl.co');
+	socket.on('data', todo);
+	socket.on('mouse', drw);
+  //socket.on('clearall',clearit);
+	button.position(width - 100, height - 100);
+	button.mousePressed(send);
 
- button = createButton('click me');
-  button.position(19, 19);
-  button.mousePressed(changeBG);
-  let inp = createInput('');
- inp.input(myInputEvent);
+	button = createButton('clear');
+	button.position(width - 100, height - 60);
+	button.mousePressed(clearit);
+
+  button = createButton('clear all');
+	button.position(width - 170, height - 60);
+	button.mousePressed(clearall);
+
+	inp = createInput('');
+	inp.input(myInputEvent);
+
+  fill(0);
+	textSize(22);
+  text(' Draw here ', 200, 200);
+
 
 }
 
-
-function todo(data){
-	console.log(c+" : "+ data.name +" : "+data.msg);
-  text(c+' : '+ data.name +' : '+data.msg,0,c*15);
+function todo(dat)
+{
+	text("{"+dat.name + '} ' + dat.msg, 60,c*10);
+	c+=2;
 }
 
 function draw()
 {
-  fill(80,0,0);
-  ellipse(mouseX,mouseY,100,50);
-  fill(90);
-  textSize(12);
-//  text('Georgia', 120, 300);
-
+  fill(0);
+	textSize(15);
+  text('made by Manthan', 120, 300);
 }
-
 
 function keyPressed()
-{entered(key);}
-function entered(key){
-
-  if(key=='Enter')
-  {
-    if(c==0){name=msg.join('');msg = [''];}
-
-     data={
-       msg:msg.join(''),
-       name:name
-           }
-        socket.emit('data',data);
-
-      console.log(c+" you : "+msg.join(''));
-      msg = [''];
-      c++;
-  }else{
-
-   append(msg, key);
-
-       }
+{
+	entered(key);
 }
-function changeBG() {
-  let val = random(255);
-  background(val,random(255),random(255));
-  entered('enter');
+
+function entered(key)
+{
+	if (key == 'Enter')
+  {
+		if (c1 === 0)
+    {
+		data = {
+			name:inp.elt.value
+		};
+		}
+
+		data.msg=inp.elt.value;
+
+		socket.emit('data', data);
+
+		console.log(c + ' you : ' + data.msg + '   u r name ' + data.name);
+
+    text(" {you} "+data.msg, 200,c*10);
+
+	  c+=2;
+
+		c1+=2;
+		msg = data.msg;
+	}
+}
+function send() {
+	let val = random(255);
+//	background(val, random(255), random(255));
+	entered('Enter');
 }
 
 function myInputEvent() {
-  console.log('you are typing: ', this.value());
+	console.log('you are typing: ');
+	//msg=this.value();
+}
+
+function clearit() {
+  background(255);
+	c = 1;
+  c1=1;
+}
+function mouseDragged() {
+  fill(80, 0, 0);
+	ellipse(mouseX, mouseY, 10, 10);
+	fill(90);
+	mouse = {
+		mx: mouseX,
+		my: mouseY
+	};
+
+	socket.emit('mouse', mouse);
+}
+
+function drw(mous) {
+	fill(30, 0, 0);
+	ellipse(mous.mx, mous.my, 10, 10);
+	console.log('drawing');
+}
+
+function clearall(){
+background(180);
+c1=1;
+c=1;
+var cn=0;
+socket.emit('clearall',cn);
+}
+
+function mousePressed(){
+ if(click==0)
+ {
+ background(255);
+ click++;
+ }
 }
