81b32e651c3681f4e1f10f73663a8db84bdbb6c9
Manthan Patil <manthanmji@gmail.com>
2024-09-25 18:15:50 +0000

    Cleanup

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 let express = require('express');
 let online = 0;
-let app = express();
+let app = express()
 
-let server = app.listen(process.env.PORT || 3000);
+let port = process.env.PORT || 3000
+let server = app.listen(port)
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-console.log('RUNING YOUR SERVER ...');
-console.log('hearing requests at localhost:3000..');
+console.log('RUNING YOUR SERVER ...')
+console.log(`hearing requests at localhost:${port}..`)
 
-let socket = require('socket.io');
-let io = socket(server);
+let socket = require('socket.io')
+let io = socket(server)
 
-io.sockets.on('connection', newConnection);
+io.sockets.on('connection', newConnection)
 
 function newConnection(socket) {
-    console.log('new connection : ' + socket.id + '  online ' + ++online);
-    io.sockets.emit('online', online);
+    console.log('new connection : ' + socket.id + '  online ' + ++online)
+    io.sockets.emit('online', online)
 
     socket.on('clearall', () => {
         socket.broadcast.emit('clearall')
     })
 
     socket.on('data', (data) => {
-        console.log(data);
-        socket.broadcast.emit('data', data);
-    });
+        socket.broadcast.emit('data', data)
+    })
 
     socket.on('mouse', (d) => {
-        socket.broadcast.emit('mouse', d);
-    });
+        socket.broadcast.emit('mouse', d)
+    })
 
     socket.on('disconnect', () => {
-        console.log('disconnected now online : ' + online);
-        online = online - 1;
-        io.sockets.emit('online', online);
-    });
+        console.log('disconnected now online : ' + online)
+        online = online - 1
+        io.sockets.emit('online', online)
+    })
 
     setInterval(() => {
-        socket.emit('online', online);
-    }, 500);
+        socket.emit('online', online)
+    }, 500)
 }
